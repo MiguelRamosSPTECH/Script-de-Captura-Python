1dae8e192c579b9eaf16e09698749979e7ab1614
MiguelRamosSPTECH <miguel.ramos@sptech.school>
2025-08-31 19:13:35 -0300

    Script com exibicão mais intuitiva para o usuario

--- script-oficial.py
+++ script-oficial.py
@@ -1,23 +1,25 @@
 import pandas as pd
 import psutil
 import time
-import os
 from datetime import datetime
 import pytz #ajusta fuso horário
+import pyfiglet
+import sys
 
 # Definir o fuso horário do Brasil
 fuso_horario_brasil = pytz.timezone('America/Sao_Paulo')
 
 dados = {
     "timestamp": [],
+    "identificao-mainframe":[],
     "uso_cpu_total_%":[], #dados_cpu
     "uso_ram_total_%":[], #uso_ram
     "swap_rate_mbs": [], #pegar_swap_rate
     # "tempo_cpu_ociosa": [], #dados_cpu
     # "cpu_io_wait_%":[],#dados_cpu
     "uso_disco_total_%":[], #uso_disco
-    "disco_iops_total": [], #pegar_iops_e_latencia
     "disco_throughput_mbs": [], #pegar_throughput
+    "disco_iops_total": [], #pegar_iops_e_latencia
     "disco_read_count":[], #pegar_iops_e_latencia
     "disco_write_count":[], #pegar_iops_e_latencia
     "disco_latencia_ms": [] #pegar_iops_e_latencia
@@ -83,10 +85,20 @@ def uso_disco():
     dados_disco = psutil.disk_usage('/').percent
     return dados_disco    
 
+def montar_msg(dado, nomeDado, metrica, limite_barra, numDivisao):
+    calculo_total_barras = int(limite_barra * (dado/numDivisao))
+    return f"{nomeDado} [{'■'* calculo_total_barras}{" "*(limite_barra - calculo_total_barras)}] {dado}{metrica}"
 
-print("Iniciando captura de dados...",datetime.now())
-print("------------------------------------------------------------------------------")
-max_barra = 25
+def carregamento():
+    for i in range(1,101):
+        sys.stdout.write("\r"+f"Carregando:  {i}%") #volta sempre pro começo da linha e fica sobrescrevendo
+        sys.stdout.flush() #nao espera buffer encher
+        time.sleep(0.05)
+    sys.stdout.write("\n")
+
+print(f"HORÁRIO AGORA = {datetime.now().strftime("%d/%m/%Y %H:%M")}")
+print(pyfiglet.figlet_format("INICIANDO..."))
+print(carregamento())
 while True:
     horario_agora = datetime.now()
     trata_data = datetime.strftime(horario_agora, "%d-%m-%Y %H:%M:%S")
@@ -99,6 +111,7 @@ while True:
     dados_disco.append(throughput)
 
     dados["timestamp"].append(trata_data)
+    dados["identificao-mainframe"].append(psutil.users()[0].name)
     dados["uso_cpu_total_%"].append(dados_cpu[2])
     dados["uso_ram_total_%"].append(uso_ram_porcentagem)
     dados["swap_rate_mbs"].append(swap_rate[2])
@@ -111,19 +124,28 @@ while True:
     dados["disco_write_count"].append(dados_disco[2])
     dados["disco_latencia_ms"].append(dados_disco[3])
 
-    print("Uso da CPU (%) =", "|" * (int(max_barra * ((dados_cpu[2]/100)))),f"{dados_cpu[2]}%")
-    print("Uso da RAM (%) =", "|" * (int(max_barra * ((uso_ram_porcentagem/100)))),f"{uso_ram_porcentagem}%")
-    print("Swap Rate (MB/s) =", "|" * (int(max_barra * ((swap_rate[2]/100)))),f"{swap_rate[2]} MB/s")
-    print("Tempo de CPU Ociosa (s) =", "|" * (int(dados_cpu[0]* ((dados_cpu[0]/100)))),f"{dados_cpu[0]} s")
-    # print("Tempo de CPU I/O Ociosa (s) =", "|" * (int(dados_cpu[0]* ((dados_cpu[0]/100)))),f"{dados_cpu[0]} s")
-    print("Uso do Disco (%) =", "|" * (int(max_barra* ((uso_disco_porcentagem/100)))),f"{uso_disco_porcentagem} GB")
-    print("IOPS =", "|" * (int(max_barra* ((dados_disco[0]/100)))),f"{dados_disco[0]} MB/s")
-    print("Throughput do disco =", "|" * (int(max_barra* ((dados_disco[len(dados_disco)-1]/100)))),f"{dados_disco[len(dados_disco)-1]} MB/s")
-    print("Dados lidos no disco =", "|" * (int(max_barra* ((dados_disco[1]/100)))),f"{dados_disco[1]} MB/s")
-    print("Dados escritos no disco =", "|" * (int(max_barra* ((dados_disco[2]/100)))),f"{dados_disco[2]} MB/s")
-    print("Latência do disco =", "|" * (int(max_barra* ((dados_disco[3]/100)))),f"{dados_disco[3]} ms")
+    print(f"""
+        +------------------------------------------------------------------------------+
 
+        !--------IDENTIFICAÇÃO DO MAINFRAME---------!
+            {dados["identificao-mainframe"][len(dados["identificao-mainframe"])-1]}
+
+        !---------------DADOS DA CPU----------------!
+            
+            {montar_msg(dados["uso_cpu_total_%"][len(dados["uso_cpu_total_%"])-1], "Consumo da CPU", "%", 10, 100)}
+
+        !---------------DADOS DA RAM----------------!
+
+            {montar_msg(dados["uso_ram_total_%"][len(dados["uso_ram_total_%"])-1], "Consumo da RAM", "%", 10, 100)}
+            {montar_msg(dados["swap_rate_mbs"][len(dados["swap_rate_mbs"])-1], "Dados indo para memória SWAP", "MB/s", 10, 100)}
+
+        !---------------DADOS DO DASD---------------!
+            {montar_msg(dados["uso_disco_total_%"][len(dados["uso_disco_total_%"])-1], "Consumo do DASD", "%", 10, 100)}
+            {montar_msg(dados["disco_throughput_mbs"][len(dados["disco_throughput_mbs"])-1], "Throughput do DASD", "MB/s", 10, 100)}
+            {montar_msg(dados["disco_iops_total"][len(dados["disco_iops_total"])-1], "IOPS no Disco", "MB/s", 10, 100)}
+            {montar_msg(dados["disco_read_count"][len(dados["disco_read_count"])-1], "Dados lidos no DASD", "MB/s", 10, 100)}
+            {montar_msg(dados["disco_write_count"][len(dados["disco_write_count"])-1], "Dados escritos no DASD", "MB/s", 10, 100)}
+            {montar_msg(dados["disco_latencia_ms"][len(dados["disco_latencia_ms"])-1], "Latência do DASD", "ms",10, 1000)}
+    """)
     df = pd.DataFrame(dados)
     df.to_csv("dados-mainframe.csv", encoding="utf-8", sep=";", index=False)
-    
-    print("\n------------------------------------------------------------------------------\n")
