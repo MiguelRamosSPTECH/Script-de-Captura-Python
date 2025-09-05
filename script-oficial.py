adf9df0f8abe861acfd2ce07f64875a52d7f362d
MiguelRamosSPTECH <miguel.ramos@sptech.school>
2025-09-05 08:11:50 -0300

    Código completo, adicionando cpu ociosa e cpu i/o wait

--- script-oficial.py
+++ script-oficial.py
@@ -15,8 +15,8 @@ dados = {
     "uso_cpu_total_%":[], #dados_cpu
     "uso_ram_total_%":[], #uso_ram
     "swap_rate_mbs": [], #pegar_swap_rate
-    # "tempo_cpu_ociosa": [], #dados_cpu
-    # "cpu_io_wait_%":[],#dados_cpu
+    "tempo_cpu_ociosa": [], #dados_cpu
+    "cpu_io_wait":[],#dados_cpu
     "uso_disco_total_%":[], #uso_disco
     "disco_throughput_mbs": [], #pegar_throughput
     "disco_iops_total": [], #pegar_iops_e_latencia
@@ -75,11 +75,11 @@ def pegar_iops_e_latencia():
 def pegar_dados_cpu():
     # só com linux o io_wait
     cpu_dados = psutil.cpu_times_percent(interval=0.1)
-    # cpu_iowait = cpu_dados.iowait
+    cpu_iowait = cpu_dados.iowait
     cpu_idle = cpu_dados.idle
     cpu_uso_usuarios = cpu_dados.user
     cpu_uso_sistema = cpu_dados.system
-    return [cpu_idle, cpu_uso_usuarios, cpu_uso_sistema]
+    return [cpu_idle, cpu_uso_usuarios, cpu_uso_sistema, cpu_iowait]
 
 def uso_disco():
     dados_disco = psutil.disk_usage('/').percent
@@ -115,8 +115,8 @@ while True:
     dados["uso_cpu_total_%"].append(dados_cpu[2])
     dados["uso_ram_total_%"].append(uso_ram_porcentagem)
     dados["swap_rate_mbs"].append(swap_rate[2])
-    # dados["tempo_cpu_ociosa"].append(dados[0])
-    # dados["cpu_io_wait_%"].append(dados[0])
+    dados["tempo_cpu_ociosa"].append(dados_cpu[0])
+    dados["cpu_io_wait"].append(dados_cpu[3])
     dados["uso_disco_total_%"].append(uso_disco_porcentagem)
     dados["disco_iops_total"].append(dados_disco[0])
     dados["disco_throughput_mbs"].append(dados_disco[len(dados_disco)-1])
@@ -133,6 +133,8 @@ while True:
         !---------------DADOS DA CPU----------------!
             
             {montar_msg(dados["uso_cpu_total_%"][len(dados["uso_cpu_total_%"])-1], "Consumo da CPU", "%", 10, 100)}
+            {montar_msg(dados["tempo_cpu_ociosa"][len(dados["tempo_cpu_ociosa"])-1], "Tempo de CPU Ociosa", "s", 10, 100)}
+            {montar_msg(dados["cpu_io_wait_%"][len(dados["cpu_io_wait"])-1], "Tempo de CPU I/O Wait","s",10,100)}
 
         !---------------DADOS DA RAM----------------!
 
@@ -147,5 +149,5 @@ while True:
             {montar_msg(dados["disco_write_count"][len(dados["disco_write_count"])-1], "Dados escritos no DASD", "MB/s", 10, 100)}
             {montar_msg(dados["disco_latencia_ms"][len(dados["disco_latencia_ms"])-1], "Latência do DASD", "ms",10, 1000)}
     """)
-    # df = pd.DataFrame(dados)
-    # df.to_csv("dados-mainframe.csv", encoding="utf-8", sep=";", index=False)
+    df = pd.DataFrame(dados)
+    df.to_csv("dados-mainframe.csv", encoding="utf-8", sep=";", index=False)
